25456c9fcc533d6745e98d5d63f5af2c9f5b77f2
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2024-01-22 00:06:04 +0530

    Update AccessToken.js

--- client/client/src/Components/AccessToken.js
+++ client/client/src/Components/AccessToken.js
@@ -8,7 +8,7 @@ const AccessToken = (props) => {
     const navigate = useNavigate();
 
     const handleClick = ()=>{
-        axios.post("http://localhost:5000/checkToken", {token}).then(
+        axios.post("https://image-wave-server.vercel.app/checkToken", {token}).then(
           response=>{
               if(response.data === 'Valid')
               {
