5bc31417abe97cb7f7a7552742ce707185ca86b6
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2024-02-01 23:18:44 +0530

    Update TryImageWave.js

--- client/client/src/Components/TryImageWave.js
+++ client/client/src/Components/TryImageWave.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef} from 'react';
 import { saveAs } from 'file-saver'
 import axios from 'axios'
 import send from '../images/send.png';
