6bb2f389319694d47f70e9966f4310754720cbd7
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2025-12-16 22:14:19 +0530

    Update main.py

--- server/main.py
+++ server/main.py
@@ -15,7 +15,7 @@ db = client.mydatabase
 
 
 from huggingface_hub import InferenceClient
-client = InferenceClient(token= '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', model="dataautogpt3/OpenDalleV1.1")
+client = InferenceClient(provider="hf-inference", token= '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', model="stabilityai/stable-diffusion-xl-base-1.0")
 app = Flask(__name__)
 CORS(app)
 
